8f0cc3ba19f97156bd1d48c69e0812a356d0a2f5
maddiestrange <maddie.strange@gmail.com>
2019-09-26 20:51:11 -0500

    initial setup

---
+++ react-redux-app/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Reducer } from './reducers';
+
+const store = createStore(Reducer, applyMiddleware(thunk, logger));
+
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+    </Provider>, 
+    document.getElementById('root'));
